cb9ae159dde49173a0ad0389b6c9b3643b683134
Scherebart <bart@scherebart.com>
2023-04-30 21:01:50 +0200

    Make-up good-looking like buttons

--- TODO.md
+++ TODO.md
@@ -7,7 +7,8 @@
 4. [OK] Add search feature to the SECOND TAB
 5. Add button in the SEARCH to LIKE movie
 6. Add button to REMOVE liked movie in the first tab
-7. Show LIKED STATUS in the search tab
+7. Show LIKED STATUS on the searched movies
+8. Show modal with details on clicking searched movie
 
 
 # Further improvements
--- web/src/MovieBriefTile.vue
+++ web/src/MovieBriefTile.vue
@@ -2,20 +2,78 @@
 const props = defineProps({
   movie: Object
 })
+
+import HeartFilled from './heart-filled-svgrepo-com.svg'
+import HeartEmptyThick from './heart-empty-thick-svgrepo-com.svg'
+
 </script>
+
+<style>
+.like-button {
+  position: absolute;
+  margin: 1rem;
+  bottom: 0;
+  right: 0;
+  border-radius: 50%;
+  border-width: 0.15rem;
+  border-color: #444;
+  background-color: #eaeaeae0;
+}
+
+.like-button:hover {
+  border-color: #555;
+}
+
+.like-button.empty {
+  opacity: 0.65;
+  transition: all 0.4s ease-out;
+}
+
+.like-button.empty:hover {
+  background-color: #eaeaeaff;
+  opacity: 1;
+  box-shadow: 0rem 0rem 0.8rem 0.2rem #aaa;
+  transition: all 0.15s ease-in;
+}
+
+
+.like-button.empty svg path {
+  stroke: #333;
+}
+
+
+.like-button.filled {
+  opacity: 1;
+  background-color: #eaeaeaff;
+  border-color: #eaeaeaff;
+  border-width: 0.1rem;
+  box-shadow: 0rem 0rem 0.8rem 0.4rem #aaa;
+}
+
+.like-button.filled svg {
+  fill: #b32fed;
+}
+</style>
 
 <template>
   <div class="column is-3">
     <div class="card">
       <div class="card-image">
         <figure class="image ">
-          <img :src="movie.Poster">
+          <img v-if="movie.Poster" :src="movie.Poster">
+          <img v-else :src="imageMoviePlaceholder">
+          <button class="button like-button is-medium filled">
+          <!-- <button class="button like-button is-medium empty"> -->
+            <HeartFilled class="icon"></HeartFilled>
+            <!-- <HeartEmptyThick class="icon empty"></HeartEmptyThick> -->
+          </button>
         </figure>
       </div>
       <div class="card-header">
-        <div class="card-header-title">
+        <p class="card-header-title">
           {{ movie.Title }}
-        </div>
+        </p>
+
       </div>
       <div class="card-content">
         <p>Year: {{ movie.Year }}</p>
